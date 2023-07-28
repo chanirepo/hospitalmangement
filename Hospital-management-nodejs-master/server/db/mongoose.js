8674e6e59182e8948b8c25f65e01a43265060b9d
chanirepo <140132762+chanirepo@users.noreply.github.com>
2023-07-28 11:09:38 +0530

    Update mongoose.js

--- Hospital-management-nodejs-master/server/db/mongoose.js
+++ Hospital-management-nodejs-master/server/db/mongoose.js
@@ -1,9 +1,49 @@
-var mongoose = require ('mongoose');
+// Require the MongoDB Node.js driver
+const { MongoClient } = require("mongodb");
 
+// Connection URL and database name
+const url = "mongodb://139.5.248.188:27017";
+const dbName = "myDatabase";
 
-mongoose.Promise = global.Promise;
+// Function to connect to the MongoDB server
+async function connectToDatabase() {
+  try {
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const db = client.db(dbName);
+    return db;
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    return null;
+  }
+}
 
-//change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+// Function to insert a document into a collection
+async function insertDocument(collectionName, document) {
+  const db = await connectToDatabase();
+  if (!db) return;
 
-module.exports = {mongoose};
+  const collection = db.collection(collectionName);
+
+  try {
+    const result = await collection.insertOne(document);
+    console.log("Document inserted:", result.insertedId);
+  } catch (error) {
+    console.error("Error inserting document:", error);
+  }
+}
+
+// Function to find documents in a collection based on a query
+async function findDocuments(collectionName, query) {
+  const db = await connectToDatabase();
+  if (!db) return;
+
+  const collection = db.collection(collectionName);
+
+  try {
+    const cursor = await collection.find(query);
+    const documents = await cursor.toArray();
+    console.log("Found documents:", documents);
+  } catch (error) {
+    console.error("Error finding documents:", error);
+  }
+}
